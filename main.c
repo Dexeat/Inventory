cdcb15ee5bd8b71aa6519b72e7be1d9b8febd755
Dexeat <Dexeat@users.noreply.github.com>
2020-01-27 12:09:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 struct Items
 {
     char nom[40];
@@ -10,6 +11,7 @@ struct Items
 
 
 typedef struct Items objet;
+typedef struct Items inventory;
 
 void affichage(objet liste[6]){
 
@@ -23,10 +25,34 @@ void affichage(objet liste[6]){
 
 }
 
+int menu(int choix,inventory inv[999],objet liste[6],int*gold){
+    if (choix == 1)
+    {
+        affichage(liste);
+    }
+    
+    if (choix == 2)
+    {
+        achat(*gold,liste,inv);
+    }
+    if (choix == 3)
+    {
+        inventaire(inv);
+    }
+    
+}
+
+void achat(int *gold,objet liste[6],inventory inv[999]){}
+
+void inventaire(inventory inv[999]){}
+
 
 int main(void){
     //liste d'objet
+    int achat = 1;
     int nb_base = 999;
+    int gold = 100000000;
+    int choix;
     
 
     objet carte = {"carte",nb_base,10};
@@ -37,8 +63,17 @@ int main(void){
     objet talisment = {"talisment",1,6000};
     
     objet liste_objet[6] = {carte,potion,pomme,epee,bouclier,talisment};
+    inventory inv[999];
 
     affichage(liste_objet);
+
+    while (achat)
+    {
+        printf("Menu: 1 Magasin, 2 Achat, 3 Inventaire");
+        scanf("%d\n",&choix);
+        menu(choix,inv,liste_objet,gold);
+    }
+    
 
     return 0;
 }
