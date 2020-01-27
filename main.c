682f696a1f4b027989d1e1a2e678bbaed2895d5e
Dexeat <Dexeat@users.noreply.github.com>
2020-01-27 11:28:41 +0100

    INIT

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct Items
+{
+    char nom;
+    int nombre;
+    int prix;
+};
+
+
+typedef struct Items objet;
+
+
+
+int main(void){
+
+
+    return 0;
+}
